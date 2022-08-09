9f209e99eacbc35d554985f293d298c178354163
Maxxbay <maxxbay@gmail.com>
2022-08-09 10:54:26 +0200

    oi

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,21 +1,3 @@
-// import Player from '@vimeo/player';
-// import { throttle } from 'lodash';
-
-// const iframe = document.querySelector('iframe');
-// const player = new Player(iframe);
-
-// player.on(
-//   'timeupdate',
-//   throttle(element => {
-//     localStorage.setItem('videoplayer-current-time', element.seconds);
-//   }, 1000)
-// );
-
-// player
-//   .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
-//   .catch(function (error) {
-//     console.error(error);
-//   });
 import Player from '@vimeo/player';
 import { throttle } from 'lodash';
 
